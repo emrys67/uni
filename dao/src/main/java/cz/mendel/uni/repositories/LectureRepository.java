acfbc450a29f9688431a652144e3a558317b1daa
emrys <emriesy@gmail.com>
2021-10-27 23:31:00 +0200

    forgotten pom and repository from previous commit

---
+++ dao/src/main/java/cz/mendel/uni/repositories/LectureRepository.java
@@ -0,0 +1,7 @@
+//package cz.mendel.uni.dao.repositories;
+//
+//import cz.mendel.uni.dao.entities.Lecture;
+//import org.springframework.data.repository.CrudRepository;
+//
+//public interface LectureRepository extends CrudRepository<Lecture, Long> {
+//}
