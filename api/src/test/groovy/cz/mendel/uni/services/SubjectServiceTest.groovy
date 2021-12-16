e20b6375d8147de477cd89c62b7fb20f95d7802f
emrys67 <emriesy@gmail.com>
2021-12-16 07:27:28 +0100

    edit pom.xml so tests are running from terminal and generating jacoco report; add few tests

--- api/src/test/groovy/cz/mendel/uni/services/SubjectServiceTest.groovy
+++ api/src/test/groovy/cz/mendel/uni/services/SubjectServiceTest.groovy
@@ -1,6 +1,7 @@
 package cz.mendel.uni.services
 
 import cz.mendel.uni.entities.Subject
+import cz.mendel.uni.entities.Teacher
 import cz.mendel.uni.repositories.SubjectRepository
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.test.context.SpringBootTest
@@ -56,5 +57,28 @@ class SubjectServiceTest extends Specification {
         subjectService.deleteById(1)
         then:
         1 * subjectRepository.deleteById(1)
+    }
+
+    def "Teacher is added to Subject.teachers when addTeacher(Subject, Teacher) is used"() {
+        given:
+        ArrayList<Teacher> arrayList = new ArrayList<Teacher>()
+        Subject subjectStub = Stub()
+        subjectStub.getTeachers() >> arrayList
+        when:
+        subjectService.addTeacher(subjectStub, new Teacher())
+        then:
+        arrayList.get(0) == new Teacher()
+    }
+
+    def "Teacher is removed from Subject.teachers when deleteTeacher(Subject, Teacher) is used"() {
+        given:
+        ArrayList<Teacher> arrayList = new ArrayList<Teacher>()
+        arrayList.add(new Teacher())
+        Subject subjectStub = Stub()
+        subjectStub.getTeachers() >> arrayList
+        when:
+        subjectService.deleteTeacher(subjectStub, new Teacher())
+        then:
+        arrayList.isEmpty()
     }
 }
